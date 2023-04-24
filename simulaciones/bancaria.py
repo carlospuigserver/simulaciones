9643e84284e1180d265d9681e715efa90a277f86
carlospuigserver <cpuiggut@myuax.com>
2023-04-24 17:08:59 +0200

    primer intento

--- simulaciones/bancaria.py
+++ simulaciones/bancaria.py
@@ -23,3 +23,28 @@ class TestCuentaBanco(unittest.TestCase):
     def test_obtener_saldo(self):
         cuenta=CuentaBanco(saldo_inicial=100)
         self.assertEqual(cuenta.obtener_saldo(),100)
+
+
+
+class TestCuentaBancoIntegracion(unittest.TestCase):
+    def test_procesos_ingreso_retiro(self):
+        cuenta=CuentaBanco(saldo_inicial=100)
+        procesosIngreso= [100]*40 + [50]*20 + [20]*60
+        procesosRetiro= [100]*40 + [50]*20 + [20]*60
+
+        def proceso_ingreso(cuenta,cantidad):
+            cuenta.ingresar(cantidad)
+
+        def proceso_retiro(cuenta,cantidad):
+            cuenta.retirar(cantidad)
+
+
+        import multiprocessing
+        pool=multiprocessing.Pool()
+        pool.map(proceso_ingreso,[cuenta]*len(procesosIngreso),procesosIngreso)
+        pool.map(proceso_retiro,[cuenta]*len(procesosRetiro),procesosRetiro)
+        self.assertEqual(cuenta.obtener_saldo(),100)
+
+if __name__ == '__main__':
+    unittest.main()
+
